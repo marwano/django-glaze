84e0ba1df865fd411009f696ec13f342b5d5cc76
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-07-09 00:48:20 +0300

    added UpdateMixin class and choices function

---
+++ glaze/utils/models.py
@@ -0,0 +1,9 @@
+
+class UpdateMixin(object):
+    def update(self, **kwargs):
+        for name in kwargs:
+            setattr(self, name, kwargs[name])
+            self.save()
+
+def choices(items):
+    return tuple((i, i) for i in items.split())
