7bde0721929257b953f629e48b21c56b8070d40d
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-09-07 17:43:51 +0300

    fixed multiple save bug in UpdateMixin

--- glaze/utils/models.py
+++ glaze/utils/models.py
@@ -3,7 +3,7 @@ class UpdateMixin(object):
     def update(self, **kwargs):
         for name in kwargs:
             setattr(self, name, kwargs[name])
-            self.save()
+        self.save()
 
 def choices(items):
     return tuple((i, i) for i in items.split())
