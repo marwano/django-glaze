f06616d02a39ce71cc27579ab65869c6aff47fdc
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-09-08 07:42:44 +0300

    added logic to automatically calculate the fetch count

--- glaze/utils/reloader.py
+++ glaze/utils/reloader.py
@@ -45,7 +45,7 @@ def fetch_worker(url, stats):
 
 class ReloaderTrick(Trick):
     def __init__(
-            self, groups, htaccess, fetch_url=None, fetch_count=0,
+            self, groups, htaccess, url=None, fetch_count=0,
             log_config=None, wait=2, signal=SIGTERM, group_format='(wsgi:%s)',
             **kwargs):
         super(ReloaderTrick, self).__init__(**kwargs)
@@ -57,11 +57,11 @@ class ReloaderTrick(Trick):
         self.htaccess = htaccess
         self.wait = wait
         self.signal = signal
-        self.fetch_url = fetch_url
+        self.url = url
         self.fetch_count = fetch_count
         self.group_format = group_format
         self.last_reload = 0
-        self.fetch_stats = Queue()
+        self.proc_count = 0
         self.save_htaccess(self.current)
 
     def save_htaccess(self, group):
@@ -74,6 +74,10 @@ class ReloaderTrick(Trick):
         items = get_pid_list()
         items = [(pid, process_name(pid, ignore_errors=True)) for pid in items]
         pids = [pid for pid, cmd in items if cmd and cmd[0].strip() == name]
+        self.proc_count = len(pids)
+        if not pids:
+            logging.error('no processes found with name %r' % name)
+            return
         logging.info('killing %r with pids %r' % (name, pids))
         for i in pids:
             try:
@@ -85,16 +89,18 @@ class ReloaderTrick(Trick):
     def fetch(self):
         workers = []
         stats = []
-        for i in range(self.fetch_count):
-            args = (self.fetch_url, self.fetch_stats)
+        statsq = Queue()
+        count = self.fetch_count or self.proc_count
+        for i in range(count):
+            args = (self.url, statsq)
             worker = Thread(target=fetch_worker, args=args)
             worker.start()
             workers.append(worker)
         for worker in workers:
             worker.join()
-            stats.append(self.fetch_stats.get())
+            stats.append(statsq.get())
         stats = ', '.join(["%dms" % i for i in sorted(stats)])
-        logging.info('fetched %r with stats %s' % (self.fetch_url, stats))
+        logging.info('fetched %r with stats %s' % (self.url, stats))
 
     def reload(self):
         new = next(self.groups)
