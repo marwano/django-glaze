0c046cbb657152bbd7681fa77cd72b30f3c3768b
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-09-15 21:02:27 +0300

    added make_fieldset() function

--- glaze/utils/models.py
+++ glaze/utils/models.py
@@ -1,9 +1,16 @@
 
+
 class UpdateMixin(object):
     def update(self, **kwargs):
         for name in kwargs:
             setattr(self, name, kwargs[name])
         self.save()
 
+
 def choices(items):
     return tuple((i, i) for i in items.split())
+
+
+def make_fieldset(fields, name=None):
+    fields = fields.split() if isinstance(fields, basestring) else fields
+    return (name, dict(fields=fields))
