c45e578667511a6fde79b861b625bdcee5d83de0
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2013-09-16 07:24:51 +0300

    renamed make_fieldset() to fieldset()

--- glaze/utils/models.py
+++ glaze/utils/models.py
@@ -11,6 +11,6 @@ def choices(items):
     return tuple((i, i) for i in items.split())
 
 
-def make_fieldset(fields, name=None):
+def fieldset(fields, name=None, **field_options):
     fields = fields.split() if isinstance(fields, basestring) else fields
-    return (name, dict(fields=fields))
+    return (name, dict(fields=fields, **field_options))
