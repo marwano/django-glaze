ab26448260ab3a9d3979713379078df4b8d10558
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2014-03-22 22:13:54 +0300

    added CSS and a navbar to GlazeAdminSite

---
+++ glaze/templatetags/glaze.py
@@ -0,0 +1,14 @@
+
+from django import template
+from django.core import urlresolvers
+from django.conf import settings
+from utile import resolve
+
+register = template.Library()
+
+
+@register.filter()
+def glaze_site_lookup(path, name):
+    namespace = urlresolvers.resolve(path).namespace
+    site = resolve(settings.GLAZE_SITE_LOOKUP[namespace])
+    return getattr(site, name)
