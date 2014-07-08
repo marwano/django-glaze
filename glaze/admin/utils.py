609591f7b83c7010fd9ed3b2b4502c138457bda7
Marwan Alsabbagh <marwan.alsabbagh@gmail.com>
2014-07-08 13:09:01 +0300

    set __name__ attribute in foreign_fields

--- glaze/admin/utils.py
+++ glaze/admin/utils.py
@@ -9,6 +9,7 @@ def foreign_fields(*args):
         if isinstance(i, six.string_types) and '__' in i:
             names = i.split('__')
             accessor = lambda obj: reduce(getattr, names, obj)
+            accessor.__name__ = names[-1]
             accessor.short_description = deslugify(names[-1])
             accessor.admin_order_field = i
             items.append(accessor)
